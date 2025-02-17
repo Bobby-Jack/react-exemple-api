7d171fc4bc5da1ab8995ec3c12f64ad94c17bb5f
Bobby-Jack <pigpongproduction@gmail.com>
2025-02-17 10:09:00 +0100

    first comments

--- src/api/fetchApi.js
+++ src/api/fetchApi.js
@@ -1,6 +1,11 @@
+
+//cette fonction lance une requête API et retourne la réponse sous format json
+
 export const getAllProductsData = async () => {
+    //fetch permet d'envoyer une requête (GET par défaut)
+    //elle est await car son résultat provient d'internet, une source extérieur au programme
     const response = await fetch("https://dummyjson.com/products");
-  
+    //si la réponse est mauvaise une erreur est lancé (elle sera catché par le try dans Home.jsx)
     if (!response.ok) {
       throw new Error(`HTTP error: Status ${response.status}`);
     }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,15 @@ import { useState, useEffect } from 'react'
 import { getAllProductsData } from '../api/fetchApi'
 
 export default function Home() {
-    //ce state conserve les données 
+    //ce state conserve les données de tout les produits
     const [productData, setProductData] = useState(null)
+    //ce state conserve une erreur survenu lors de la requête API
     const [errorApi, setErrorApi] = useState(null)
 
-
+    //le useEffect aura pour but de lancé la requête au chargement du composant 
     useEffect(() => {
+      //cette fonction asynchrone s'occupe de lancer la fonction getAllProductsData
+      // et de stocker le résultat dans productData ou errorApi (en cas de problème)
       async function fetchDummyData(){
         try{
             const response = await getAllProductsData()
@@ -16,9 +19,6 @@ export default function Home() {
       
         }catch(error){
             console.log(error.message);
-            
-            setErrorApi(error)
-            console.log(errorApi);
             setProductData(null)
         }
     
@@ -26,19 +26,18 @@ export default function Home() {
 
         fetchDummyData()
       
-
-    
-      
     }, [])
     
   return (
     <div>
     <h1>All products</h1>  
     {
+        //affichage conditionel du message d'erreur
         errorApi && <h2 style={{color : "red"}}>{errorApi.message}</h2>  
     }
     <div>
         {
+            //affichage conditionelle des données une fois qu'elles sont chargées
             productData ?
             <ul>
             {
